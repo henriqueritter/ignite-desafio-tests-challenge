5cea97a99bd4ce7fe60a343d61c79a046f595574
Henrique Ritter <henrique-ritter@hotmail.com>
2022-02-27 14:59:12 -0300

    Init Create Statement unit test

---
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -0,0 +1,11 @@
+import { InMemoryStatementsRepository } from '../../repositories/in-memory/InMemoryStatementsRepository';
+import { InMemoryUsersRepository } from '../../../users/repositories/in-memory/InMemoryUsersRepository';
+
+import { CreateUserUseCase } from '../../../users/useCases/createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from '../../../users/useCases/authenticateUser/AuthenticateUserUseCase';
+import { CreateStatementUseCase } from '../createStatement/CreateStatementUseCase';
+
+
+describe("Create Statement", () => {
+  beforeEach(() => { })
+})
