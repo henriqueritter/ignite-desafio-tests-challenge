95d9e22233857401f19204cdb2a1d434cf69aa9e
Henrique Ritter <henrique-ritter@hotmail.com>
2022-02-28 20:34:56 -0300

    Created create user integration test

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,52 @@
+import request from 'supertest';
+
+//import createConnection from '../../../../database';
+
+import { app } from '../../../../app';
+import { Connection, createConnection } from 'typeorm';
+
+let connection: Connection;
+
+describe('Create User Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
+  it("Should be able to create a new user", async () => {
+    const response = await request(app).post('/api/v1/users')
+      .send({ name: "admin", email: "admin@test.com", password: "1234" });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a user with an email which already exists", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "admin", email: "admin@test.com", password: "1234"
+    });
+
+    const response = await request(app).post("/api/v1/users").send({
+      name: "admin", email: "admin@test.com", password: "1234"
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toEqual("User already exists");
+  });
+
+  it("Should be able to create a new user with a new email address", async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "user", email: "user@test.com", password: "1234"
+    });
+
+    const response = await request(app).post("/api/v1/users").send({
+      name: "new user", email: "new.user@test.com", password: "1234"
+    });
+
+    expect(response.status).toBe(201);
+  })
+})
