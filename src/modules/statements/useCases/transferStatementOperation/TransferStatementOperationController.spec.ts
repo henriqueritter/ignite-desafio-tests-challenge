8ecd04f654312e093f24e951ef5a226b08940fc7
henriqueritter <henrique-ritter@hotmail.com>
2022-04-25 21:17:40 -0300

    [tests] created integration tests file of transferStatement

---
+++ src/modules/statements/useCases/transferStatementOperation/TransferStatementOperationController.spec.ts
@@ -0,0 +1,11 @@
+import { Connection, createConnection } from 'typeorm';
+import { app } from '../../../../app';
+import request from 'supertest';
+
+let connection: Connection;
+
+describe("Transfer Statement Controller", () => {
+  beforeAll(async () => { });
+
+  afterAll(async () => { });
+})
